9dc5c12aab98981a87f8ede2e6e92c8678c7743d
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-03-10 19:03:41 -0500

    Add files via upload

---
+++ Mortal Kombat Picker/mkActions.js
@@ -0,0 +1,192 @@
+document.getElementById('kitana').addEventListener('click', princess);
+document.getElementById('scorp').addEventListener('click', wraith);
+document.getElementById('subz').addEventListener('click', gMaster);
+document.getElementById('rep').addEventListener('click', acidSpit);
+document.getElementById('kang').addEventListener('click', shaolin);
+document.getElementById('sonya').addEventListener('click', specOps);
+document.getElementById('cyrax').addEventListener('click', cyberAssassin);
+document.getElementById('jax').addEventListener('click', bionicSoldier);
+document.getElementById('kano').addEventListener('click', mercScum);
+document.getElementById('jade').addEventListener('click', royalGuard);
+document.getElementById('sheeva').addEventListener('click', shokan);
+document.getElementById('shangtsung').addEventListener('click', sorceror);
+
+function princess() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.toggle('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function wraith() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.toggle('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function gMaster() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.toggle('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function acidSpit() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden'); 
+    document.getElementById('repDesc').classList.toggle('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function shaolin() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.toggle('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function specOps() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.toggle('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function cyberAssassin() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.toggle('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function bionicSoldier() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.toggle('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function mercScum() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('kanoDesc').classList.toggle('hidden');
+}
+
+function royalGuard() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.toggle('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden'); 
+}
+
+function shokan() {
+    document.getElementById('shangDesc').classList.add('hidden');
+    document.getElementById('sheevDesc').classList.toggle('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
+
+function sorceror() {
+    document.getElementById('shangDesc').classList.toggle('hidden');
+    document.getElementById('sheevDesc').classList.add('hidden');
+    document.getElementById('jadeDesc').classList.add('hidden');
+    document.getElementById('jaxDesc').classList.add('hidden');
+    document.getElementById('cyraxDesc').classList.add('hidden');
+    document.getElementById('sonyaDesc').classList.add('hidden');
+    document.getElementById('kangDesc').classList.add('hidden');
+    document.getElementById('repDesc').classList.add('hidden');
+    document.getElementById('scorpDesc').classList.add('hidden');
+    document.getElementById('subzDesc').classList.add('hidden');
+    document.getElementById('kitDesc').classList.add('hidden');
+    document.getElementById('kanoDesc').classList.add('hidden');
+}
